855f710007d8d5bf08c010ea5424056598e62920
Pluk Zwaal <Plukky.zwaal@gmail.com>
2023-02-27 11:23:49 +0100

    .

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,8 +1,17 @@
 <template>
-    <div class="max-w-sm rounded overflow-hidden shadow-lg text-center m-auto mt-40">
-        <img class="h-64 w-64 m-auto" :src="product.image" alt="Sunset in the mountains">
-        <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">{{ product.title }} ${{ product.price }}</div>
+    <div class="flex items-center justify-center h-screen">
+        <div class="max-w-sm rounded overflow-hidden shadow-lg text-center">
+            <img class="h-64 w-64 m-auto" :src="product.image" alt="Sunset in the mountains">
+            <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">{{ product.title }}</div>
+                {{ product.description }}
+                <div class="px-6 pt-4 pb-2">
+                    <span
+                        class="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">${{
+                            product.price
+                        }}</span>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -11,6 +20,10 @@
 const { id } = useRoute().params
 const uri = `https://fakestoreapi.com/products/${id}`
 const { data: product } = await useFetch(uri)
+
+if(!product.value){
+    throw createError({statusCode: 404, statusMessage: 'Product not found'})
+}
 </script>
 
 <style scoped>
